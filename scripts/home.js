e0b41608166e3b1bf6afa8406cec76fcf646ac5f
Enne <cheyenne03hug@gmail.com>
2026-07-29 19:51:36 -0400

    allow for click of linkedlist box to show linkedlist page

--- scripts/home.js
+++ scripts/home.js
@@ -3,3 +3,9 @@ let arrayListBox = document.getElementById("arrayLists");
 arrayLists.addEventListener("click", function () {
     window.location.href = "data-struc-pages/arraylists.html"
 });
+
+let linkedListBox = document.getElementById("linkedLists");
+
+linkedListBox.addEventListener("click", function () {
+    window.location.href = "data-struc-pages/linkedlists.html"
+});
