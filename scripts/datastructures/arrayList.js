a3122052c0d2d721f28213d6e9fbe0422c723c97
Enne <cheyenne03hug@gmail.com>
2026-08-08 19:25:41 -0400

    add animation to arraylist interactive visual

--- scripts/datastructures/arrayList.js
+++ scripts/datastructures/arrayList.js
@@ -1,7 +1,9 @@
 let arrayListDiv = document.getElementById("visualDiv");
+let arrayListDiv2 = document.getElementById("visualDiv2");
 
 let sizeMethodButton = document.getElementById("resize");
 let increaseByN = true;
+let increasByNButtonText = "Increase by 5";
 
 let nContainer = document.getElementById("change-n-container");
 let nValueInput = document.getElementById("nValueInput");
@@ -11,6 +13,8 @@ let addItemInput = document.getElementById("addItemInput");
 let addItemButton = document.getElementById("addItemButton");
 
 let arrayList = new ArrayList();
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function addDisplayItem(id) {
     let arrayItem = document.createElement("p");
@@ -22,7 +26,51 @@ function addDisplayItem(id) {
     arrayListDiv.append(arrayItem);
 }
 
-function addArrayItem (item) {
+function secondDisplayItem(id) {
+    let arrayItem = document.createElement("p");
+    arrayItem.classList.add("array-item");
+
+    let fullId = "array2-" + id;
+    arrayItem.id = fullId;
+
+    arrayListDiv2.append(arrayItem);
+}
+
+async function change(size, capacity) {
+    arrayListDiv2.classList.toggle("hidden");
+
+    for (let i = size; i < capacity; i++) {
+        secondDisplayItem(i);
+    }
+
+    for (let i = 0; i < size; i++) {
+        await delay(800);
+        let smallArrayElement = document.getElementById("array-" + i);
+        let bigArrayElement = document.getElementById("array2-" + i);
+
+        bigArrayElement.innerText = smallArrayElement.innerText;
+    }
+
+    await delay(800);
+    arrayListDiv.classList.toggle("hidden");
+
+    for (let i = size; i < capacity; i++) {
+        addDisplayItem(i);
+    }
+
+    arrayListDiv2.classList.toggle("hidden");
+    arrayListDiv.classList.toggle("hidden");
+
+    for (let i = 0; i < size; i++) {
+        let bigArrayElement = document.getElementById("array2-" + i);
+
+        bigArrayElement.innerText = "";
+    }
+
+    await delay(800);
+}
+
+async function addArrayItem (item) {
     let size = arrayList.getSize();
 
     let id = "array-" + size;
@@ -31,10 +79,7 @@ function addArrayItem (item) {
 
     if (message == ErrorMessage.CHANGE) {
         let capacity = arrayList.getCapacity();
-
-        for (let i = size; i < capacity; i++) {
-            addDisplayItem(i);
-        }
+        await change(size, capacity);
     }
 
     let displayArrayItem = document.getElementById(id);
@@ -44,6 +89,9 @@ function addArrayItem (item) {
 nValueIButton.addEventListener("click", function() {
     let newN = nValueInput.value;
     arrayList.changeN(newN);
+
+    increasByNButtonText = "Increase by " + newN;
+    sizeMethodButton.innerText = increasByNButtonText;
     nValueInput.value = "";
 });
 
@@ -53,23 +101,21 @@ sizeMethodButton.addEventListener("click", function() {
         sizeMethodButton.innerText = "Double";
     } else {
         increaseByN = true;
-        sizeMethodButton.innerText = "Increase by n";
+        sizeMethodButton.innerText = increasByNButtonText;
     }
 
     nContainer.classList.toggle("hidden");
     arrayList.changeReallocType();
 });
 
-addItemButton.addEventListener("click", function () {
+addItemButton.addEventListener("click", async function () {
     let value = addItemInput.value;
-
-    if (value.indexOf(",") >= 0) {
-        let array = value.split(",");
-        for (let i of array) {
-            addArrayItem(i);
-        }
-    } else {
-        addArrayItem(value);
+    
+    let array = value.split(",");
+    for (let i of array) {
+        await addArrayItem(i);
+        addItemInput.value = addItemInput.value.substring(addItemInput.value.indexOf(",") + 1);
+        await delay(800);
     }
 
     // addArrayItem(addItemInput.value);
